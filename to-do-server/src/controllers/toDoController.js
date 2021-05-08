12bdf644a628636134921d1992492fa315dc4d28
Lilit <devlilitbandeira@gmail.com>
2021-05-08 19:24:12 -0300

    ajustes e revisão no servidor feito na aula

--- to-do-server/src/controllers/toDoController.js
+++ to-do-server/src/controllers/toDoController.js
@@ -1,6 +1,17 @@
 const tarefasJson = require("../models/tarefas.json")
 const fs = require("fs")
 
+const getAll = (req, res)=>{
+  res.status(200).send(tarefasJson)
+}
+
+const getById = (req, res) =>{
+  const id = req.params.id
+  const tarefa = tarefasJson.find(i => i.id == id)
+
+  res.status(200).send(tarefa)
+}
+
 const createTask = (req, res) => {
   const descricao = req.body.descricao
   const nome = req.body.nomeColaborador
@@ -19,30 +30,31 @@ const createTask = (req, res) => {
       return res.status(424).send({message: err})
     }
   })
+}
 
-  const deleteTask = (req, res) => {
-    const idRecebido = req.params.id
-    const tarefa = tarefasJson.find(i => i.id === idRecebido)
-    const indice = tarefasJson.indexOf(tarefa)
+const deleteTask = (req, res) => {
+  const idRecebido = req.params.id
+  const tarefa = tarefasJson.find(i => i.id === idRecebido)
+  const indice = tarefasJson.indexOf(tarefa)
 
-    tarefasJson.splice(indice, 1)
+  tarefasJson.splice(indice, 1)
 
-    fs.writeFile("./src/models/tarefas.json", JSON.stringify(tarefasJson), 'utf8', function(err) {
-      if(err) {
-        return res.status(424).send({message: err})
-      }
-    })
+  fs.writeFile("./src/models/tarefas.json", JSON.stringify(tarefasJson), 'utf8', function(err) {
+    if(err) {
+      return res.status(424).send({message: err})
+    }
+  })
 
-    res.status(200).json([{
-      'mensagem': 'Tarefa deletada com sucesso',
-      tarefasJson
-    }])
-  }
+  res.status(200).json([{
+    'mensagem': 'Tarefa deletada com sucesso',
+    tarefasJson
+  }])
 
-  res.status(200).send(novaTarefa)
 }
 
 module.exports = {
+  getAll,
+  getById,
   createTask,
   deleteTask
 }
