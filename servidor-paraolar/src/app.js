b57bb9062014b05b53a97098e7c2c8e9d02c5277
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2021-05-08 20:51:42 -0300

    Modificado o aquivo app.js

--- servidor-paraolar/src/app.js
+++ servidor-paraolar/src/app.js
@@ -4,7 +4,8 @@ const app = express();
 const index = require("./routes/index");
 const contatos = require("./routes/contatosRoutes")
 
-//app.use(express.json());
+// Transforma tudo que o cliente digitou em JSON
+app.use(express.json());
 
 // Rota raiz
 app.use("/", index);
