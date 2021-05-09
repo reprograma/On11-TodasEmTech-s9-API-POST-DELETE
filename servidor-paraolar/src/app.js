7e72334d423e6ed36e3b0ea125627808e4ec7836
Larissa <lcsmonteiiro@gmail.com>
2021-05-09 03:52:50 -0300

    Para o lar resolvido

--- servidor-paraolar/src/app.js
+++ servidor-paraolar/src/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 const index = require("./routes/index")
 
 const contatos = require("./routes/paraOLarRoutes")
--- servidor-paraolar/src/routes/paraOLarRoutes.js
+++ servidor-paraolar/src/routes/paraOLarRoutes.js
@@ -4,5 +4,7 @@ const controller = require("../controllers/paraOLarController")
 
 router.get("/", controller.getAll)
 router.get("/:id",controller.getById)
+router.post("/adicionar",controller.createTask)
+router.delete("/:id",controller.deleteTask)
 
 module.exports = router
