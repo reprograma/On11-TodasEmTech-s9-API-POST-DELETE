6952474c3448def665afbc5f848e7368cf246373
Lilit <devlilitbandeira@gmail.com>
2021-05-09 09:47:12 -0300

    correção do cors

--- servidor-paraolar/src/app.js
+++ servidor-paraolar/src/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json())
 
 const index = require('./routes/index')
