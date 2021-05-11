ee646afd2dbb984dbc46d589cdf3217e5ff62dad
Mariana Lima <marilimabrito13@gmail.com>
2021-05-10 22:05:34 -0300

    Atividade para o lar semana 9 resolvida

---
+++ to-do-server/server.js
@@ -0,0 +1,6 @@
+const app = require("./src/app")
+const PORT = 3030
+
+app.listen(PORT, () =>{
+    console.log(`Bebê o APP ta rosando na porta ${PORT}`)
+})
---
+++ to-do-server/src/routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = express.Router()
+
+
+router.get("/", (request, response)=>{
+    response.status(200).json({
+        "titulo": "To-do Api - Reprograma",
+        "version": "1.0.0",
+        "mensagem": "bem vindo"
+
+    })
+
+})
+
+
+module.exports = router 
