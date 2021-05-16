d2d4f7e058e76694d8bb1510d3fcd4063432271b
littlejane <janeramerindo@gmail.com>
2021-05-15 21:09:01 -0300

    Exercicios da Semana Realizadosgit status

--- servidor-paraolar/server.js
+++ servidor-paraolar/server.js
@@ -1,3 +1,3 @@
 const app = require('./src/App')
-const PORT = 8080
+const PORT = 8081
 app.listen(PORT, () => console.log(`> ${PORT}`))
--- servidor-paraolar/src/App.js
+++ servidor-paraolar/src/App.js
@@ -2,9 +2,10 @@ const express = require('express');
 //const path = require('path')
 const index = require('./routes')
 const contatos = require('./routes/contatinhosRoutes');
-
+const cors = require('cors')
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use('/', index)
 app.use('/contatos', contatos)
